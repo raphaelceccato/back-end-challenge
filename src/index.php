f34c8e068b82076884d6534778784d9052eeb0d7
Raphael <raphaelceccato@hotmail.com>
2024-11-03 10:14:09 -0300

    update index.php: linter fixes and change in docs

--- src/index.php
+++ src/index.php
@@ -4,13 +4,13 @@
  *
  * PHP version 7.4
  *
- * Este será o arquivo chamado na execução dos testes automátizados.
+ * Este será o arquivo chamado na execução dos testes automatizados.
  *
  * @category Challenge
  * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
+ * @author   Raphael Ceccato Pauli <raphaelcpauli@gmail.com>
  * @license  http://opensource.org/licenses/MIT MIT
- * @link     https://github.com/apiki/back-end-challenge
+ * @link     https://github.com/raphaelceccato/back-end-challenge
  */
 declare(strict_types=1);
 
@@ -29,41 +29,45 @@ $controller = new ExchangeController();
 $router = new Router();
 
 
-$router->get("/^\/exchange\/(.*)\/(.*)\/(.*)\/(.*)$/i", function(array $params) use($controller) {
-    $value = isset($params[1]) ? $params[1] : null;
-    $from = isset($params[2]) ? $params[2] : null;
-    $to = isset($params[3]) ? $params[3] : null;
-    $amount = isset($params[4]) ? $params[4] : null;
+$router->get(
+    "/^\/exchange\/(.*)\/(.*)\/(.*)\/(.*)$/i", function (array $params) use ($controller) {
+        $value = isset($params[1]) ? $params[1] : null;
+        $from = isset($params[2]) ? $params[2] : null;
+        $to = isset($params[3]) ? $params[3] : null;
+        $amount = isset($params[4]) ? $params[4] : null;
 
-    $res = new RouteResponse();
-    $res->content_type = "application/json";
+        $res = new RouteResponse();
+        $res->content_type = "application/json";
 
-    try {
-        $result = $controller->exchange($value, $from, $to, $amount);
-        $res->content = json_encode($result);
+        try {
+            $result = $controller->exchange($value, $from, $to, $amount);
+            $res->content = json_encode($result);
+        }
+        catch (ControllerException $ex) {
+            $res->code = $ex->getCode();
+            $res->content = json_encode([ "error" => $ex->getMessage() ]);
+        }
+        catch (Exception $ex) {
+            $res->code = 500;
+            $res->content = json_encode([ "error" => "Erro interno do servidor" ]);
+            throw $ex;
+        }
+
+        $res->build();
     }
-    catch (ControllerException $ex) {
-        $res->code = $ex->getCode();
-        $res->content = json_encode([ "error" => $ex->getMessage() ]);
+);
+
+
+
+$router->get(
+    "/.*/", function (array $params) {
+        $res = new RouteResponse();
+        $res->code = 400;
+        $res->content_type = "application/json";
+        $res->content = json_encode([ "error" => "Requisição inválida" ]);
+        $res->build();
     }
-    catch (Exception $ex) {
-        $res->code = 500;
-        $res->content = json_encode([ "error" => "Erro interno do servidor" ]);
-        throw $ex;
-    }
-
-    $res->build();
-});
-
-
-
-$router->get("/.*/", function(array $params) {
-    $res = new RouteResponse();
-    $res->code = 400;
-    $res->content_type = "application/json";
-    $res->content = json_encode([ "error" => "Requisição inválida" ]);
-    $res->build();
-});
+);
 
 
 
