0f305777c563533ad7a751240e281c08eeec3ac1
Raphael <raphaelceccato@hotmail.com>
2024-11-03 09:59:15 -0300

    update index.php: add routes

--- src/index.php
+++ src/index.php
@@ -16,3 +16,55 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+
+use App\Controllers\ExchangeController;
+use App\Utils\ControllerException;
+use App\Utils\Router;
+use App\Utils\RouteResponse;
+
+
+$controller = new ExchangeController();
+
+
+$router = new Router();
+
+
+$router->get("/^\/exchange\/(.*)\/(.*)\/(.*)\/(.*)$/i", function(array $params) use($controller) {
+    $value = isset($params[1]) ? $params[1] : null;
+    $from = isset($params[2]) ? $params[2] : null;
+    $to = isset($params[3]) ? $params[3] : null;
+    $amount = isset($params[4]) ? $params[4] : null;
+
+    $res = new RouteResponse();
+    $res->content_type = "application/json";
+
+    try {
+        $result = $controller->exchange($value, $from, $to, $amount);
+        $res->content = json_encode($result);
+    }
+    catch (ControllerException $ex) {
+        $res->code = $ex->getCode();
+        $res->content = json_encode([ "error" => $ex->getMessage() ]);
+    }
+    catch (Exception $ex) {
+        $res->code = 500;
+        $res->content = json_encode([ "error" => "Erro interno do servidor" ]);
+        throw $ex;
+    }
+
+    $res->build();
+});
+
+
+
+$router->get("/.*/", function(array $params) {
+    $res = new RouteResponse();
+    $res->code = 400;
+    $res->content_type = "application/json";
+    $res->content = json_encode([ "error" => "Requisição inválida" ]);
+    $res->build();
+});
+
+
+
+$router->process_request();
