df748de8fded5f0666f3dcf687972714bbfa5065
Raphael <raphaelceccato@hotmail.com>
2024-11-03 09:58:06 -0300

    update ExchangeController.php: add parameter checking

--- src/Controllers/ExchangeController.php
+++ src/Controllers/ExchangeController.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 
+use App\Utils\ControllerException;
+
+
 class ExchangeController {
     private $symbols = [
         "BRL" => "R$",
@@ -13,6 +16,15 @@ class ExchangeController {
 
 
     public function exchange(?string $value, ?string $from, ?string $to, ?string $rate) {
+        if (!is_numeric($value)
+        || !is_numeric($rate)
+        || !isset($this->symbols[$from])
+        || !isset($this->symbols[$to])
+        || $value < 0
+        || $rate < 0) {
+            throw new ControllerException(400, "Requisição inválida");
+        }
+
         return [
             "valorConvertido" => floatval($value) * floatval($rate),
             "simboloMoeda" => $this->symbols[$to]
